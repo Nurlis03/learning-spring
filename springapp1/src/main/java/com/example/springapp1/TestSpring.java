7c13b4024e0b00de4c2e905d23233612777f5ed5
Nurlis <kimbiletovnurlis03@gmail.com>
2023-12-09 00:03:33 +0600

    learned bean lifecycle

--- springapp1/src/main/java/com/example/springapp1/TestSpring.java
+++ springapp1/src/main/java/com/example/springapp1/TestSpring.java
@@ -8,13 +8,27 @@ public class TestSpring {
             "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
         // Music music = context.getBean("musicBean", Music.class);
         // MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        // MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        // MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        // firstMusicPlayer.playMusic();
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        // boolean comparasion = firstMusicPlayer == secondMusicPlayer;
+
+        // System.out.println(comparasion);
+        // System.out.println(firstMusicPlayer);
+        // System.out.println(secondMusicPlayer);
+
+        // firstMusicPlayer.setVolume(10);
+
+        // System.out.println(firstMusicPlayer.getVolume());
+        // System.out.println(secondMusicPlayer.getVolume());
+        // System.out.println(firstMusicPlayer.getName());
+        // System.out.println(firstMusicPlayer.getVolume());
         context.close();
     }
 }
